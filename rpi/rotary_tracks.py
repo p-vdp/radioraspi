19458eaf71fe2412195a1b90407e01ee4e6487d3
p-vdp <pvonderporten@gmail.com>
2024-01-31 12:05:36 -0800

    added working scriptios

--- rpi/rotary_tracks.py
+++ rpi/rotary_tracks.py
@@ -1,8 +1,56 @@
 from time import sleep
 
+from mpd import MPDClient
 from RPi import GPIO
 
-dt = 12
-clk = 14
 
-sleep(1)
+debounce = 0.01  # seconds
+clk = 27
+dt = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter = 0
+rotation_direction = None
+clk_last_state = GPIO.input(clk)
+
+try:
+    while True:
+        GPIO.wait_for_edge(clk, GPIO.BOTH)
+        # get state
+        clk_state = GPIO.input(clk)
+        dt_state = GPIO.input(dt)
+
+        if clk_state != clk_last_state:
+            if dt_state != clk_state:
+                counter += 1
+                rotation_direction = "clockwise"
+            else:
+                counter -= 1
+                rotation_direction = "counterclockwise"
+
+        # do mpd command
+        if rotation_direction:
+            try:
+                # print(counter, rotation_direction)
+                client = MPDClient()
+                client.connect("localhost", 6600)
+                status = client.status()
+                # print(status['playlistlength'], status['song'])
+                if rotation_direction == "clockwise" and int(status['song']) < int(status['playlistlength']) - 1:
+                    client.next()
+                if rotation_direction == "counterclockwise":
+                    client.previous()
+            finally:
+                # print(status['playlistlength'], status['song'])
+                client.disconnect()
+
+        # reinitialize
+        rotation_direction = None
+        clk_last_state = clk_state
+        sleep(debounce)
+finally:
+    GPIO.cleanup()
+
---
+++ rpi/toggle_shutdown.py
@@ -0,0 +1,38 @@
+from os import system
+from time import sleep
+
+from RPi import GPIO
+
+
+def command(cmd):
+    print(cmd)
+    system(cmd)
+
+
+reset_sw = 16
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(reset_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+        print("initial wait")
+        GPIO.wait_for_edge(reset_sw, GPIO.FALLING)
+
+        print("edge detected")
+        samples = 0
+        for i in range(300):
+            sleep(0.01)  # iter for 3 secs
+            state = GPIO.input(reset_sw)
+            if not i % 100:
+                print(int(i / 100) + 1, samples, state)
+            if state == 0:
+                samples += 1
+        print(samples)
+
+        if samples < 250:
+            print("no action, wait\n")
+            sleep(2.0)
+        else:
+            command("sudo shutdown")
+finally:
+    GPIO.cleanup()
