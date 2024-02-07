a4d011542443cad9675e8a38a5fb30aa7c217bc9
p-vdp <pvonderporten@gmail.com>
2024-02-07 00:27:14 -0800

    shutdown working

--- rpi/toggle_shutdown.py
+++ rpi/toggle_shutdown.py
@@ -9,30 +9,34 @@ def command(cmd):
     system(cmd)
 
 
-# reset_sw = 16
+sw = 16
+wait = 5  # seconds
+cmd = "sudo shutdown now"
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(reset_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
     while True:
         print("initial wait")
-        GPIO.wait_for_edge(reset_sw, GPIO.FALLING)
+        GPIO.wait_for_edge(sw, GPIO.FALLING)
 
         print("edge detected")
         samples = 0
-        for i in range(300):
-            sleep(0.01)  # iter for 3 secs
-            state = GPIO.input(reset_sw)
+        for i in range(wait * 100):
+            sleep(0.01)  # iter for 5 secs
+            state = GPIO.input(sw)
             if not i % 100:
                 print(int(i / 100) + 1, samples, state)
             if state == 0:
                 samples += 1
         print(samples)
 
-        if samples < 250:
+        if samples < wait * 80:
             print("no action, wait\n")
             sleep(2.0)
         else:
-            command("sudo shutdown")
+            command(cmd)
+            break
 finally:
     GPIO.cleanup()
