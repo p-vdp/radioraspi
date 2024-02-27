ec6f67e82f9495488a5b684930a23ce072925765
p-vdp <pvonderporten@gmail.com>
2024-02-26 21:28:39 -0800

    pi5

---
+++ rpi/button_playpause.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+import gpiod
+from mpd import MPDClient
+
+
+BUTTON_PIN = 4
+DEBOUNCE = 1.0  # seconds
+POLLING_WAIT = 0.01  # seconds
+
+chip = gpiod.Chip('gpiochip4')
+button_line = chip.get_line(BUTTON_PIN)
+button_line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
+
+try:
+    while True:
+        button_state = button_line.get_value()
+        client = MPDClient()
+        if button_state == 0:
+            client.connect("localhost", 6600)
+            # status = client.status()
+            client.pause()
+            print("pressed")
+            sleep(DEBOUNCE)
+        sleep(POLLING_WAIT)
+finally:
+    button_line.release()
+
