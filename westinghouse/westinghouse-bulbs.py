8117165e399790519329d2993f2859e4539c9109
p-vdp <pvonderporten@gmail.com>
2024-03-30 17:45:01 -0700

    bulbs working

--- westinghouse/westinghouse-bulbs.py
+++ westinghouse/westinghouse-bulbs.py
@@ -1,5 +1,19 @@
-from utils import Gpio
+from time import sleep
+
+import utils
 
 # LED control
-led_closed = Gpio(12, "op")
-led_open = Gpio(17, "op")
+led_closed = utils.Gpio(12, "op")
+led_open = utils.Gpio(17, "op")
+
+try:
+    while True:
+        led_closed.on()
+        led_open.on()
+
+        if utils.get_mpd_status() is False:
+            led_open.blink(1)
+finally:
+    led_closed.off()
+    led_open.off()
+    utils.chip.close()
