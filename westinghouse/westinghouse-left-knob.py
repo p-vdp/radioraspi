347ae9257fa7b6e523e03e4e391ab2b2470c67e9
p-vdp <pvonderporten@gmail.com>
2024-03-31 16:03:23 -0700

    left knob service fix

--- westinghouse/westinghouse-left-knob.py
+++ westinghouse/westinghouse-left-knob.py
@@ -4,20 +4,21 @@ from mpd import MPDClient
 
 import utils
 
+
 left_knob = utils.Gpio(4, "wait")
+print("waiting")
+left_knob.wait()
+print("continuing")
 
 client = MPDClient()
-
-while True:
-    print("waiting")
-    left_knob.wait()
-    print("continuing")
-    try:
-        client.connect("localhost", 6600)
-        print("play/pause")
-        client.pause()  # noqa
-        client.disconnect()
-    except ConnectionRefusedError:
-        print("Not connected")
+try:
+    client.connect("localhost", 6600)
+    print("play/pause")
+    client.pause()  # noqa
+except ConnectionRefusedError:
+    print("Not connected")
+finally:
+    client.disconnect()
+    left_knob.line.release()
     print("sleeping")
     sleep(3)
