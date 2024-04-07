b7faff44aed7c861f4fe82809caec53b444bf557
p-vdp <pvonderporten@gmail.com>
2024-04-07 11:23:12 -0700

    refactoring

--- westinghouse/utils.py
+++ westinghouse/utils.py
@@ -1,24 +1,35 @@
 import select
 from time import sleep
 
-import gpiod
+import python_gpiod
 from mpd import MPDClient
 
 
-chip = gpiod.Chip("gpiochip4")
+CHIP = gpiod.Chip("gpiochip4")  # Raspberry Pi 5 spec
 
 
-class Gpio:
-    def __init__(self, pin_num, type: str, gpiod_chip=chip):
-        self.line = gpiod_chip.get_line(pin_num)
-        self.type = type
+class Line(gpiod.Line):
+    pass
 
-        if self.type == "ip":
+
+class Input:
+    def __init__(self, pin_num: int, io_type: str, circuit_type: str = "open"):
+        try:
+            assert pin_num.is_integer()
+            assert io_type in ["ip", "op", "wait"]
+            assert circuit_type in ["open", "closed"]
+        except AssertionError:
+            raise TypeError
+
+        self.line = CHIP.get_line(pin_num)
+        self.io_type = io_type
+
+        if self.io_type == "ip":
             raise NotImplementedError
             # self.line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
-        elif self.type == "op":
+        elif self.io_type == "op":
             self.line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-        elif self.type == "wait":
+        elif self.io_type == "wait":
             self.line.request(
                 consumer="GPOUT",
                 type=gpiod.LINE_REQ_EV_FALLING_EDGE,
@@ -28,49 +39,40 @@ class Gpio:
             raise ValueError
 
     def on(self):
-        if self.type != "op":
-            raise TypeError('Expected type output "op"')
-        else:
-            self.line.set_value(1)
+        self.line.set_value(1)
 
     def off(self):
-        if self.type != "op":
-            raise TypeError('Expected type output "op"')
+        self.line.set_value(0)
+
+    def toggle(self):
+        if self.line.get_value() == 0:
+            self.line.set_value(1)
         else:
             self.line.set_value(0)
 
-    def toggle(self):
-        if self.type != "op":
-            raise TypeError('Expected type output "op"')
-        else:
-            if self.line.get_value() == 0:
-                self.line.set_value(1)
-            else:
-                self.line.set_value(0)
-
     def blink(self, seconds: [int, float]):
-        if self.type != "op":
-            raise TypeError('Expected type output "op"')
-        else:
-            self.toggle()
-            sleep(seconds)
-            self.toggle()
-            sleep(seconds)
-            self.toggle()
+        self.toggle()
+        sleep(seconds)
+        self.toggle()
+        sleep(seconds)
+        self.toggle()
 
     def wait(self):
-        if self.type != "wait":
-            raise TypeError("Expected type wait")
-        else:  # https://stackoverflow.com/questions/76676779/problem-with-event-wait-in-python3-libgpiod
-            fd = self.line.event_get_fd()
-            poll = select.poll()
-            poll.register(fd)
-            poll.poll(None)  # wait here
-            event = self.line.event_read()
-            return event
+        """https://stackoverflow.com/questions/76676779/problem-with-event-wait-in-python3-libgpiod"""
+        fd = self.line.event_get_fd()
+        poll = select.poll()
+        poll.register(fd)
+        poll.poll(None)  # wait here
+        event = self.line.event_read()
+        return event
 
     def get_value(self):
         return self.line.get_value()
+
+
+class Output:
+    def __init__(self):
+        pass
 
 
 def get_mpd_status(host="localhost", port=6600):
--- westinghouse/westinghouse-bulbs.py
+++ westinghouse/westinghouse-bulbs.py
@@ -2,19 +2,24 @@ from time import sleep
 
 import utils
 
-led_closed = utils.Gpio(12, "op")
-led_open = utils.Gpio(17, "op")
+led_nc_yellow = utils.Gpio(13, "op")
+led_no_yellow1 = utils.Gpio(12, "op")
+led_no_yellow2 = utils.Gpio(16, "op")
+led_no_white = utils.Gpio(27, "op")
 
 try:
     while True:
-        led_closed.on()
-        led_open.on()
+        led_nc_yellow.on()
+        led_no_yellow1.off()
+        led_no_yellow2.off()
+        led_no_white.off()
 
-        if utils.get_mpd_status() is False:
-            led_open.blink(1)
-        else:
-            sleep(1)
+        # if utils.get_mpd_status() is False:
+        #   led_open.blink(1)
+        # else:
+        #   sleep(1)
 finally:
-    led_closed.off()
-    led_open.off()
-    utils.chip.close()
+    led_y1.off()
+    led_y2.off()
+    # led_y3.off()
+    led_w1.off()
