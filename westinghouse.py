9162f61b22a9d4c361bcc83cc2fec6d49d2d4e9d
p-vdp <pvonderporten@gmail.com>
2024-04-07 23:15:14 -0700

    major refactor, vol knob working

--- westinghouse.py
+++ westinghouse.py
@@ -8,114 +8,96 @@ import gpiod
 from mpd import MPDClient
 
 CHIP = gpiod.Chip("gpiochip4", gpiod.Chip.OPEN_BY_NAME)  # Raspberry Pi 5 spec
+POLLING_RATE = 0.1
 
 
 class Output:
     def __init__(
         self,
-        gpio_pin_num,
+        gpio_pin_num: int,
         normally_closed: bool = False,
-        toggle_delay: float = 1.0,
+        relay_delay: float = 0.3,
         chip: gpiod.Chip = CHIP,
     ):
-        self._value_initialized = False
-        pinctrl_get = str(run(["pinctrl", "get", "12"], capture_output=True))
-        if "| lo " in pinctrl_get:
-            self._initial_value = 0
-        else:
-            self._initial_value = 1
-
-        self._pin: int = gpio_pin_num
+        self._pin = gpio_pin_num
         self._normally_closed: bool = normally_closed
         self._normally_open: bool = not normally_closed
+        self._relay_delay = relay_delay
 
-        self.toggle_delay = toggle_delay
-
-        self._line: gpiod.Line = chip.get_line(gpio_pin_num)
+        self._line: gpiod.Line = chip.get_line(self._pin)
         self._line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 
     def __del__(self):
+        self._line.set_value(0)
         self._line.release()
 
     @property
-    def value(self):
-        if not self._value_initialized:
-            return self._initial_value
-        else:
-            return self._line.get_value()
-
-    @property
-    def is_off(self):
-        if self._normally_open is True:
-            if self.value == 0:
-                return True
-            else:
-                return False
-        else:  # normally closed
-            if self.value == 0:
-                return False
-            else:
-                return True
-
-    @property
-    def is_on(self):
-        if self._normally_open is True:
-            if self.value == 1:
-                return True
-            else:
-                return False
-        else:  # normally closed
-            if self.value == 1:
-                return False
-            else:
-                return True
+    def value(self) -> int:
+        return self._line.get_value()
 
     def toggle_on_off(self):
-        if self.value == 0:
+        if self._line.get_value() == 0:
+            sleep(self._relay_delay)
             self._line.set_value(1)
-            sleep(self.toggle_delay)
+            sleep(self._relay_delay / 2)
             return 1
         else:
+            sleep(self._relay_delay)
             self._line.set_value(0)
-            sleep(self.toggle_delay)
+            sleep(self._relay_delay / 2)
             return 0
 
-    def on(self):
-        if self.is_off:
-            return True, self.toggle_on_off()
-        else:
-            return False, None
-
-    def off(self):
-        if self.is_on:
+    def on(self) -> bool:
+        """Return True if turns on, return False if no change"""
+        value = self._line.get_value()
+        if self._normally_closed is False and value == 0:
             self.toggle_on_off()
+            return True
+        elif self._normally_closed is True and value == 1:
+            self.toggle_on_off()
+            return True
         else:
             return False
 
+    def off(self) -> bool:
+        """Return True if turns off, return False if no change"""
+        value = self._line.get_value()
+        if self._normally_closed is False and value == 1:
+            self.toggle_on_off()
+            return True
+        elif self._normally_closed is True and value == 0:
+            self.toggle_on_off()
+            return True
+        else:
+            return False
+
+    def reset(self):
+        self._line.set_value(0)
+
     def blink(self, seconds: [int, float]):
-        blink_delay = seconds - self.toggle_delay
         self.toggle_on_off()
-        sleep(blink_delay)
+        sleep(seconds)
         self.toggle_on_off()
-        sleep(blink_delay)
+        sleep(seconds)
         self.toggle_on_off()
-        return True
 
 
 class Input:
-    def __init__(
-        self, gpio_pin_num: int, normally_closed: bool = False, chip: gpiod.Chip = CHIP
-    ):
+    def __init__(self, gpio_pin_num: int, chip: gpiod.Chip = CHIP):
         self._pin: int = gpio_pin_num
-        self._normally_closed: bool = normally_closed
-        self._normally_open: bool = not normally_closed
-
         self._line: gpiod.Line = chip.get_line(gpio_pin_num)
         self._line.request(
             consumer="GPOUT",
             type=gpiod.LINE_REQ_EV_FALLING_EDGE,
             flags=gpiod.LINE_REQ_FLAG_BIAS_PULL_UP,
         )
+
+    def __del__(self):
+        self._line.release()
+
+    @property
+    def value(self) -> int:
+        return self._line.get_value()
 
     def wait(self):
         """https://stackoverflow.com/questions/76676779/problem-with-event-wait-in-python3-libgpiod"""
@@ -155,7 +137,7 @@ def mpd_startup(host="localhost", port=6600, music_path="/mnt/SDCARD"):
     try:
         client.connect(host, port)
 
-        print("Updating....")
+        print("Updating library....")
         client.update()
 
         print("Adding library to queue....")
@@ -192,53 +174,126 @@ def mpd_toggle_pause(host="localhost", port=6600):
         del client
 
 
-# async def bulb1():
-#     print(1)
-#     await asyncio.sleep(1)
-#     print(2)
-#
-#
-# async def bulb2():
-#     print(3)
-#     await asyncio.sleep(1.5)
-#     print(4)
-#
-#
-# async def main():
-#     async with asyncio.TaskGroup() as tg:
-#         t1 = tg.create_task(bulb1())
-#         t2 = tg.create_task(bulb2())
+async def mpd_status_indicator(status_bulb: int):
+    status_bulb = Output(status_bulb)
+    status_bulb.on()
+
+    while True:
+        if mpd_is_alive() is False:
+            status_bulb.blink(0.3)
+        await asyncio.sleep(POLLING_RATE)
+
+
+async def mpd_play_indicator(play_bulb: int):
+    play_bulb = Output(play_bulb)
+    while True:
+        if mpd_get_status()["state"] == "play":
+            play_bulb.on()
+        else:
+            play_bulb.off()
+        await asyncio.sleep(POLLING_RATE)
+
+
+async def mpd_volume_knob(vol_down_gpio, vol_up_gpio):
+    clk = Input(vol_down_gpio)
+    dt = Input(vol_up_gpio)
+
+    counter = 0
+    last_state = dt.value
+    while True:
+        clk_state = clk.value
+        dt_state = dt.value
+
+        if clk_state != last_state:
+            if dt_state != clk_state:
+                counter += 1
+                rotation_direction = "R"
+            else:
+                counter -= 1
+                rotation_direction = "L"
+
+            if rotation_direction == "R":
+                # print(counter, rotation_direction)
+                run("/var/www/vol.sh -up 1".split(" "))
+
+            if rotation_direction == "L":
+                # print(counter, rotation_direction)
+                run("/var/www/vol.sh -dn 1".split(" "))
+
+        last_state = clk.value
+        await asyncio.sleep(0.01)
+
+
+async def mpd_play_pause_button(play_pause_gpio):
+    pass
+
+
+async def mpd_track_knob(track_previous_gpio, track_next_gpio, feedback_gpio):
+    pass
+
+
+async def mpd_shuffle_button(track_shuffle_button, feedback_gpio):
+    pass
+
+
+async def processes(
+    nc_bulb,
+    indicator_bulb,
+    play_bulb,
+    feedback_bulb,
+    vol_down_gpio,
+    vol_up_gpio,
+    track_previous_gpio,
+    track_next_gpio,
+    play_pause_button,
+    track_shuffle_button,
+):
+    nc = Output(nc_bulb, normally_closed=True)
+    nc.off()
+    print("Started")
+
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(mpd_status_indicator(indicator_bulb))
+        tg.create_task(mpd_play_indicator(play_bulb))
+        tg.create_task(mpd_volume_knob(vol_down_gpio, vol_up_gpio))
+        tg.create_task(mpd_play_pause_button(play_pause_button))
+        tg.create_task(
+            mpd_track_knob(track_previous_gpio, track_next_gpio, feedback_bulb)
+        )
+        tg.create_task(mpd_shuffle_button(track_shuffle_button, feedback_bulb))
 
 
 if __name__ == "__main__":
     print("Starting westinghouse.py....")
 
-    print("Normally closed LEDs on GPIO 13, bulbs off")
-    led_yellow_nc = Output(13, normally_closed=True)
-    led_yellow_nc.off()
+    # print("Normally closed LEDs on GPIO 13, bulbs off")
 
-    print("Normally open LEDs on GPIO 12, bulbs on")
-    led_yellow_no1 = Output(12)
-    print(led_yellow_no1._initial_value)
-    # print(run(["pinctrl", "get", "12"]))
-    print(led_yellow_no1.on())
-    print(led_yellow_no1.off())
-    print(led_yellow_no1.toggle_on_off())
+    # print("Normally open LEDs on GPIO 12, bulbs on")
+    # led_yellow_no1 = Output(12, normally_closed=False)
+    # led_yellow_no1.on()
 
-    #
-    # print("LEDs on GPIO 16")
-    # led_yellow_no2 = Output(16)
-    # led_yellow_no2.off()
-    # print("OK", end="\r", sep="")
-    # sleep(0.5)
-    #
-    # print("LEDs on GPIO 27")
-    # led_white_no = Output(27)
-    # led_white_no.off()
-    # print("OK", end="\r", sep="")
-    # sleep(0.5)
-    # for k, v in mpd_get_status().items():
-    #     print(k, v, sep="\t\t")
-    #
-    # print("Starting processes....")
-    # asyncio.run(main())
+    # print("Normally open LEDs on GPIO 16, bulbs off")
+    # led_yellow_no2 = Output(16, normally_closed=False)
+    # print("Normally open LEDs on GPIO 27, bulbs off")
+    # led_white_no = Output(27, normally_closed=False)
+
+    print("Setting up MPD....")
+    mpd_startup()
+
+    print("Starting processes....")
+    asyncio.run(
+        processes(
+            nc_bulb=13,
+            indicator_bulb=12,
+            play_bulb=27,
+            feedback_bulb=16,
+            vol_down_gpio=23,
+            vol_up_gpio=24,
+            track_previous_gpio=4,
+            track_next_gpio=5,
+            play_pause_button=17,
+            track_shuffle_button=6,
+        )
+    )
+
+print("Exiting....")
